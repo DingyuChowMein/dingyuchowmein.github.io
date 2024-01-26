bafee8b165c30ba3ce96cf476dfc2a5d1a39b40e
Dingyu Chen <dc3117@ic.ac.uk>
2024-01-26 02:24:27 +0000

    Setup GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="200" height="200" />
         <p>
-          Test test test
+          Test test test <br/>
+          <img src="tm.png" alt="let's gooooo" width="470" height="470"/>
         </p>
         <a
-          className="App-link"
+            className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
